4c9ef95904a873d18cb19b1e2203f5a3bee4b8c8
Nina Buurman <nina.buurman@student.hku.nl>
2024-03-04 12:17:24 +0100

    Made Animation Timeline for floating intro text and doors
    *Timings still need to be fixed!!

--- VCD projectje/Assets/Scripts/DoorTrigger.cs
+++ VCD projectje/Assets/Scripts/DoorTrigger.cs
@@ -4,13 +4,15 @@ public class DoorTrigger : MonoBehaviour
 {
     public GameObject LeftDoor;
     public GameObject RightDoor;
+    public GameObject Timeline;
 
     private void OnTriggerEnter(Collider collisionInfo)
     {
         if(collisionInfo.name == "PlayerCapsule")
         {
-            LeftDoor.GetComponent<Animator>().enabled = true;
-            RightDoor.GetComponent<Animator>().enabled = true;
+            //LeftDoor.GetComponent<Animator>().enabled = true;
+            //RightDoor.GetComponent<Animator>().enabled = true;
+            Timeline.SetActive(true);
         }
     }
 }
