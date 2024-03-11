510a4724f04a26ceaa051f3525982036c9df0ea4
Nina Buurman <nina.buurman@student.hku.nl>
2024-03-11 13:59:59 +0100

    Hub doors teleport player!
    *Entering Any other door than the intended one in the Hub scene now reloads the scene
    *Music volume slider now works in every scene
    *Small changes to the text in the air in the entrance scene

---
+++ VCD projectje/Assets/Scripts/ReloadScene.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class ReloadScene : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider collisionInfo)
+    {
+        Debug.Log("collision");
+        if (collisionInfo.name == "PlayerCapsule")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+}
